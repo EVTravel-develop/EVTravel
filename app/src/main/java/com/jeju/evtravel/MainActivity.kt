9388cac6541c671452bea7606db2228d98414ed6
parksomii <somi020605@gmail.com>
2025-08-03 17:36:58 +0900

    fix: 충돌 해결 (gradle, manifest, MainActivity 등)

--- app/src/main/java/com/jeju/evtravel/MainActivity.kt
+++ app/src/main/java/com/jeju/evtravel/MainActivity.kt
@@ -1,27 +1,93 @@
 package com.jeju.evtravel
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.jeju.evtravel.navigation.BottomNavigationBar
+import com.jeju.evtravel.ui.map.KakaoMapScreen
 import com.jeju.evtravel.ui.planner.*
+import com.kakao.vectormap.KakaoMapSdk
+import com.kakao.vectormap.utils.MapUtils
 
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val plannerViewModel by viewModels<PlannerViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // 카카오 키 해시 출력
+        Log.d("KeyHash", MapUtils.getHashKey(this))
+        // Kakao SDK 초기화
+        KakaoMapSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+
         super.onCreate(savedInstanceState)
+
+        // 현재 위치 정보를 가져 오기 위한 fusedLocationClient 초기화
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         setContent {
-            EVTravelApp(plannerViewModel)
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // 모든 화면을 관리하는 MainScreen 컴포저블을 호출
+                    MainScreen(fusedLocationClient, plannerViewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(
+    fusedLocationClient: FusedLocationProviderClient,
+    plannerViewModel: PlannerViewModel
+) {
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(navController = navController)
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            NavHost(navController = navController, startDestination = "map") {
+                // 홈 탭: 지도 화면
+                composable("map") {
+                    KakaoMapScreen(fusedLocationClient)
+                }
+
+                // 플래너 탭: EVTravelApp (플래너 관련 NavHost)
+                composable("plannerTab") {
+                    EVTravelApp(plannerViewModel)
+                }
+
+                // 마이 탭
+                composable("my") {
+                    Text(text = "마이 페이지")
+                }
+            }
         }
     }
 }
@@ -64,7 +130,13 @@ fun EVTravelApp(viewModel: PlannerViewModel) {
         }
 
         // 방금 생성된 플랜 날짜를 강조해서 보여줄 때 (start, end 파라미터 포함)
-        composable("planList/{start}/{end}") { backStackEntry ->
+        composable(
+            route = "planList/{start}/{end}",
+            arguments = listOf(
+                navArgument("start") { type = NavType.StringType },
+                navArgument("end") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
             val start = backStackEntry.arguments?.getString("start")
             val end = backStackEntry.arguments?.getString("end")
             PlanListScreen(
