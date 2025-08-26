1179e82a7bed9f318b01eb0c8c212cf21b49b9cc
parksomii <somi020605@gmail.com>
2025-08-26 18:25:53 +0900

    build: 앱 등록을 위한 버전 코드 및 이름 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.jeju.evtravel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
