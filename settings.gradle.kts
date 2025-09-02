13f0dee313aea40c1a4b1c896a17bfe45003e12b
kch12 <gaemkyu4@gmail.com>
2025-09-02 19:30:13 +0900

    fix: 카카오 맵 라이브러리 추가 및 패키지 이름 정의

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         // 카카오 맵 저장소 URL 지정
+        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/repository/kakaomap-releases/")}
         maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
     }
 }
