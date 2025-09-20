03dd58244e1b58b95a309301a7892ef84af5757a
parksomii <somi020605@gmail.com>
2025-09-20 18:24:44 +0900

    build: 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = "com.jeju.evtravel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.2.1"
+        versionCode = 14
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
